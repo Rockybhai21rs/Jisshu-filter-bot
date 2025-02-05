6a6e01920b9d412a158d7d7e4dd88da093f39821
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-02-05 11:40:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,9 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Real_Pirates">Movie Pirates</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Real_Pirates">Movie Pirates</a>」</b>
+    <blockquote><b><a href="https://t.me/Real_Pirates">{file_name}</a></b></blockquote>
+    <blockquote><b></b>@Real_Pirates</blockquote>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
